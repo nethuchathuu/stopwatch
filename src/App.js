5d96314f2fcc722b9a3d8beac3be2d845566583c
nethuchathuu <155764628+nethuchathuu@users.noreply.github.com>
2024-11-04 00:36:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+    if (running) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else if (!running) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [running]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="stopwatch-container">
+      <div className="stopwatch-button"></div>
+      <div className="time-display">
+        <h1>Stopwatch</h1>
+        <span>{"0" + Math.floor((time / 60000) % 60)}; </span>
+        <span>{"0" + Math.floor((time / 1000) % 60)}; </span>
+        <span>{"0" + Math.floor((time / 10) % 100)}; </span>
+      </div>
+      <div className="buttons">
+        {running ? (
+          <button
+            onClick={() => {
+              setRunning(false);
+            }}
+          >
+            Stop
+          </button>
+        ) : (
+          <button
+            onClick={() => {
+              setRunning(true);
+            }}
+          >
+            Start
+          </button>
+        )}
+        <button
+          onClick={() => {
+            setTime(0);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
